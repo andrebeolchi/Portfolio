23509c1057e61548ac8f4087fc47d33856a7e0f4
andrebeolchi <rm84163@fiap.com.br>
2021-09-13 20:38:17 -0300

    Criando Repositórios e Add Arquivos do React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Cabecalho from './components/cabecalho/Cabecalho'
+
+function App() {
+  return (
+    <div className="App">
+        <Cabecalho/>
+    </div>
+  );
+}
+
+export default App;
