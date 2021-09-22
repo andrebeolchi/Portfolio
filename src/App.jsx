3cfea1d05492e60e0df7f2734fd0e5686480f238
andrebeolchi <rm84163@fiap.com.br>
2021-09-21 22:14:48 -0300

    Criando tela principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+import "./components/fontsAwesomeIcons"
 import Cabecalho from './components/cabecalho/Cabecalho'
+import Banner from './components/banner/Banner'
+import { dom } from '@fortawesome/fontawesome-svg-core'
+dom.watch()
 
 function App() {
   return (
     <div className="App">
         <Cabecalho/>
+        <Banner/>
     </div>
   );
 }
