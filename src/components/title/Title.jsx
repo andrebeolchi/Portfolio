e05e19e24d5ecd0663fbc79877c1cc237f4d0c3b
andrebeolchi <rm84163@fiap.com.br>
2021-09-22 22:48:59 -0300

    Adicionando H1 na Página

---
+++ src/components/title/Title.jsx
@@ -0,0 +1,11 @@
+import './Title.css'
+import React from 'react'
+
+export default function Banner() {
+
+    return (
+        <div class="card">
+            <h1>“Feito de cicatriz e gratidão pelas tempestades que passei, sigo na luta <span>sempre</span> por dias melhores.”</h1>
+        </div>
+    )
+}
