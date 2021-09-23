d6dbf4f87a6534f32a9ea8208f61b60fc357282e
andrebeolchi <rm84163@fiap.com.br>
2021-09-22 22:48:36 -0300

    Modificando CSS da NavBar

--- src/components/cabecalho/Cabecalho.jsx
+++ src/components/cabecalho/Cabecalho.jsx
@@ -1,15 +1,19 @@
 import "./Cabecalho.css"
 import React, { useState } from 'react'
 
-export default function Cabecalho() {
 
+export default function Cabecalho() {
     return (
         <header>
-            <nav>
-                <a href="#" className="">Sobre</a>
-                <a href="#" className="">Projetos</a>
-                <a href="#" className="">Contatos</a>
-                <a href="#" className="button">Download CV</a>
+            <nav className="sticky">
+                <div className="divPages">
+                    <a href="#" className="">Sobre</a>
+                    <a href="#" className="">Projetos</a>
+                    <a href="#" className="">Contatos</a>
+                </div>
+                <div>
+                    <a href="#" className="button">Download CV</a>
+                </div>
             </nav>
         </header>
     )
