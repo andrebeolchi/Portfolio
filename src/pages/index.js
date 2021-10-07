e8008bd1feeed36d54712a8d7e79f44733cfb9c3
andrebeolchi <rm84163@fiap.com.br>
2021-10-07 13:36:08 -0300

    Adicionando Formacao Academica

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import InfoSection from '../components/InfoSection'
 import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour} from '../components/InfoSection/Data'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
+import Graduation from '../components/Graduation'
 
 const Home = () => {
 
@@ -19,6 +20,7 @@ const Home = () => {
             <Sidebar isOpen={isOpen} toggle={toggle} />
             <Navbar toggle={toggle} />
             <HeroSection/>
+            <Graduation/>
             <InfoSection {...homeObjOne}/>
             <InfoSection {...homeObjTwo }/>
             <InfoSection {...homeObjThree }/>
